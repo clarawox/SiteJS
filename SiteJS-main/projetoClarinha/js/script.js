bf1bffdd6d553064e6eefcbbe2bc136e3609cf2f
clarawox <113148777+clarawox@users.noreply.github.com>
2023-02-28 22:28:41 -0300

    Add files via upload

--- SiteJS-main/projetoClarinha/js/script.js
+++ SiteJS-main/projetoClarinha/js/script.js
@@ -60,13 +60,47 @@ console.log(clientes.length); //conta a quantidade de linhas na array
 
 
 for(var i=0; i < clientes.length; i++){
+    //Captura a quantidade encomendada
     var qtd = clientes[i].querySelector(".qtd").textContent
     //console.log(qtd)
-    var val = parseFloat(clientes[i].querySelector(".valor").textContent)
+
+    //Captura o valor unitário
+    var val = clientes[i].querySelector(".valor").textContent
     //console.log(val)
-    var valTotal = qtd*val
-    console.log(valTotal)
-    
-    clientes[i].querySelector(".valor").textContent = val.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})
-    clientes[i].querySelector(".total").textContent = valTotal.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})
+
+    //Pega o nome
+    var nome = clientes[i].querySelector(".nome").textContent
+    //Verifica se a quantidade recebida é válida
+    if(isNaN(val)&&isNaN(qtd)){
+        //altera a cor da fonte da linha inteira para vermelho
+        clientes[i].style.color="darkred"
+        //altera a cor do fundo da linha inteira para vermelho
+        clientes[i].style.backgroundColor="lightcoral"
+        clientes[i].querySelector(".qtd").textContent = "Quantidade inválida";
+        clientes[i].querySelector(".valor").textContent = "Valor  inválido";
+        console.log("O valor e a quantidade inseridos pelo(a) cliente "+nome+" são inválidos")
+    }
+    else{
+        if(isNaN(val)){
+            clientes[i].querySelector(".valor").textContent = "Valor  inválido";
+            //muda apenas a cor da fonte do valor
+            clientes[i].querySelector(".valor").style.color="red"
+            console.log("O valor inserido pelo(a) cliente "+nome+" é inválido")
+        }
+
+        else if (isNaN(qtd)){
+            clientes[i].querySelector(".qtd").textContent = "Quantidade inválida";
+            //muda apenas a cor do fundo da quantidade
+            clientes[i].querySelector(".qtd").style.backgroundColor="lightcoral"
+            console.log("A quantidade inserida pela cliente "+nome+" é inválida")
+        }
+        else{
+         //Calcula o valor total
+        var valTotal = qtd*val
+        //console.log(valTotal)
+        clientes[i].querySelector(".valor").textContent = parseFloat(val).toLocaleString('pt-BR', {style:'currency', currency:'BRL'})
+        clientes[i].querySelector(".total").textContent = valTotal.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})
+        }
+    }
 }
+
