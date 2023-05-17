298c017f9cdbb87b4954de8573bde85138ed4321
clarawox <113148777+clarawox@users.noreply.github.com>
2023-05-16 22:32:53 -0300

    Add files via upload

--- projetoClarinha/js/insumos/adicionarInsumo.js
+++ projetoClarinha/js/insumos/adicionarInsumo.js
@@ -1,16 +1,14 @@
-var insumos = document.querySelectorAll(".insumo")
+// var insumos = document.querySelectorAll(".insumo")
 
-for(var i=0; i<insumos.length; i++){
-    var insu = insumos[i].querySelector(".insumo").textContent;
+// for(var i=0; i<insumos.length; i++){
+//     var insu = insumos[i].querySelector(".insumo").textContent;
 
-    var quantidade = insumos[i].querySelector(".quantidade").value;
+//     var quantidade = insumos[i].querySelector(".quantidade").textContent;
 
-    var quantidadeEmba = insumos[i].querySelector(".quantEmba").textContent;
+//     var quantidadeEmba = insumos[i].querySelector(".quantEmba").textContent;
 
-    var valor = insumos[i].querySelector(".valor").textContent;
-}
-
-
+//     var valor = insumos[i].querySelector(".valor").textContent;
+// }
 
 
 
@@ -21,29 +19,29 @@ botao.addEventListener("click", function (event){
 
     var form = document.querySelector("#FormInsumo");
     var insumo = obtemInsumo(form);
-    var validacao = validaInsumo(insumo);
+    //var validacao = validaInsumo(insumo);
     
-    if (validacao.length > 0) {
-        exibeMensagemErro(validacao);
+    /*if (validacao.length > 0) {
+        exibeErros(validacao);
         return;
-    }
+    }*/
 
-    addInsumoNaTabela(tabela);
+    addInsumoNaTabela(insumo);
     form.reset();
     document.getElementById("erro").innerHTML = "";
 })
 
 function addInsumoNaTabela(insumo){
     var insumoTr = montaLinha(insumo);
-    var tabela = document.querySelector("#tabelaInsumo");
-    insumo.appendChild(insumoTr);
+    var tabela = document.querySelector("#dados");
+    tabela.appendChild(insumoTr);
 }
 
 function obtemInsumo(form){
     var insumo = {
         insu: form.insumo.value,
         quantidade: form.quantidade.value,
-        qtdEmba: form.quatEmba.value,
+        qtdEmba: form.quantEmba.value,
         valor: form.valor.value,
     }
     return insumo;
@@ -51,17 +49,19 @@ function obtemInsumo(form){
 
 function montaLinha(insumo){
     var linha = document.createElement("tr");
+    linha.classList.add('insumo');
 
-    linha.appendChild(montaColuna(insumo.insu, "insumo"));
+    linha.appendChild(montaColuna(insumo.insu, "nomeInsumo"));
     linha.appendChild(montaColuna(insumo.quantidade, "qtd"));
-    linha.appendChild(montaColuna(insumo.qtdEmba, "qtdEmba"));
-    linha.appendChild(montaColuna(formataValor(insumo.valor, "valor")));
+    linha.appendChild(montaColuna(insumo.qtdEmba, "qtdEmbalagem"));
+    linha.appendChild(montaColuna(insumo.valor, "val"));
 
     return linha;
 }
 
 function montaColuna(dados, classe){
     var coluna = document.createElement("td");
+
     coluna.textContent = dados;
     coluna.classList.add(classe);
 
@@ -69,13 +69,13 @@ function montaColuna(dados, classe){
 }
 
 function validaInsumo(insumo){
-
     var errosInsu = [];
 
-    if(!validaQuant(insumo.quantidade)){
+    if(!validaQuant(insumo.quantidade, insumo.qtdEmba)){
         errosInsu.push("Quantidade inválida, por favor insira um número maior do que zero e menor que a quantidade na embalagem");
     }
-
+    console.log(insumo.quantidade);
+    console.log(insumo.qtdEmba);
     return errosInsu;
 }
 
--- projetoClarinha/js/insumos/calculaLucro.js
+++ projetoClarinha/js/insumos/calculaLucro.js
@@ -1,13 +1,50 @@
-function calcularCustos(){
-    var nomeProduto = document.getElementById('nome-produto').value;
-    var quantidade = parseFloat(document.getElementById('quantidade').value);
-    var quantidadeEmbalagem = parseFloat(document.getElementById('quantidade-embalagem').value);
-}
+var btnAdd= document.getElementById("calcular_valor");
+var custoTotal = 0;
+btnAdd.addEventListener("click", function(){
+    var soma = 1;
+    
+    var insumos = document.querySelectorAll('.insumo');
+    
+    insumos.forEach(insumo => {
+        
+        console.log(insumo);
+        var quantidade = insumo.querySelector('.qtd').textContent;
+        var quantidadeEmbalagem = insumo.querySelector('.qtdEmbalagem').textContent;
+        var valor = insumo.querySelector('.val').textContent;
+        
+        custoTotal = calTotal(parseInt(quantidade), parseInt(quantidadeEmbalagem), parseInt(valor));
+        console.log(custoTotal);
+        soma += parseFloat(custoTotal);
 
-function validarQtd(quantidade, quantidadeEmbalagem){
-    if(!isNaN(quantidade)&&quantidade<=quantidadeEmbalagem){
+    });
+    console.log(soma);
+
+})
+
+/*function calcularCustos(){
+    var quantidade = document.getElementById('quantidade').value;
+    var quantidadeEmbalagem = document.getElementById('quantEmba').value;
+    var valor = document.getElementById('valor').value;
+    var insumos = document.querySelectorAll('.insumo'); 
+
+    for (var i=0; i<insumos.lenght; i++){
+        var custoTotal = calTotal(quantidade, quantidadeEmbalagem, valor) + custoTotal;
+
+        console.log("oioi" + custoTotal)
+    }
+
+}*/
+
+function validaQuant(qtde, qtdEmba){
+    if(!isNaN(qtde) && qtde<=qtdEmba){
         return true;
     }else{
         return false;
     }
+}
+
+function calTotal(qtd, qtdEmba, val){
+    var total = 0;
+    total = (qtd/qtdEmba)*val;
+    return total;
 }
